3b992b3d1b6bd99ede8be12c6660eb91bb84f52d
bruno.goncalves4 <bruno.goncalves4@ITELABD03>
2022-08-04 18:58:04 -0300

    finalizado

--- CadastroPessoa/Program.cs
+++ CadastroPessoa/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SqlClient;
 
@@ -16,9 +17,14 @@ namespace CadastroPessoa
             AtualizarNovo,
             CadastrarTelefone,
             AtualizarTelefone,
-            ExcluirTelefone
+            ExcluirTelefone,
+            ListarTelefone,
+            ListarTelefonePorPessoa,
+            MostrarQuantidadeDeTelefone,
+            MostraQuantidadeDeTelefonePorPessoa
         }
-        
+        List<Pessoa> pessoas = new List<Pessoa>(); 
+
         static void Main(string[] args)
         {
 
@@ -34,12 +40,17 @@ namespace CadastroPessoa
                 Console.WriteLine("- 5 -> Atualização personalizada:");
                 Console.WriteLine("- 6 -> Cadastrar telefone:");
                 Console.WriteLine("- 7 -> Atualizar telefone:");
+                Console.WriteLine("- 8 -> Excluir telefone:");
+                Console.WriteLine("- 9 -> Listar todos os telefones:");
+                Console.WriteLine("- 10 -> Listar todos os telefones por pessoa:");
+                Console.WriteLine("- 11 -> Listar quantidade de telefones cadastrados:");
+                Console.WriteLine("- 12 -> Listar quantidade de telefones cadastrados por pessoa:");
                 Console.WriteLine("- 0 -> Sair:");
                 opcao = (Opcao)Convert.ToInt32(Console.ReadLine());
 
                 if (opcao == Opcao.Cadastrar)
                 {
-                    
+
                     var pessoa = CadastrarPessoa();
                     if (pessoa != null)
                     {
@@ -60,9 +71,9 @@ namespace CadastroPessoa
                     Console.WriteLine("-> Informe o Id da pessoa a ser atualizada: ");
                     int id = Convert.ToInt32(Console.ReadLine());
                     var pessoa = AcharPessoaPeloId(id);
-                    if (pessoa  != null && pessoa.Id == id)
+                    if (pessoa != null && pessoa.Id == id)
                     {
-                        Console.WriteLine("-> Informe o novo nome para " + pessoa.Nome );
+                        Console.WriteLine("-> Informe o novo nome para " + pessoa.Nome);
                         pessoa.Nome = Console.ReadLine();
                         Console.WriteLine("-> Informe o novo CPF:");
                         pessoa.Cpf = Console.ReadLine();
@@ -90,8 +101,13 @@ namespace CadastroPessoa
                         int confirmacao = Convert.ToInt32(Console.ReadLine());
                         if (confirmacao == 1)
                         {
-
-                            ExcluirPessoa(id);
+                            var telefone = AcharTelefonePeloIdPessoa(id);
+                            if (telefone != null)
+                            {
+                                ExcluirTelefoneIdPessoa(telefone.IdPessoa);
+                                ExcluirPessoa(id);
+                            }
+                            
                         }
                         else
                         {
@@ -121,7 +137,7 @@ namespace CadastroPessoa
                     {
                         coluna = 3;
                     }
-                    if(campo != "RG" && campo != "CPF" && campo != "NOME")
+                    if (campo != "RG" && campo != "CPF" && campo != "NOME")
                     {
                         Console.WriteLine("-> Campo não encontrado!");
                         return;
@@ -131,7 +147,7 @@ namespace CadastroPessoa
                     Console.WriteLine("-> Informe o Id do cadastro:");
                     int id = Convert.ToInt32(Console.ReadLine());
                     Pessoa pessoa = new Pessoa();
-                    pessoa.AtualizarPersonalizado(coluna, valor,id);
+                    pessoa.AtualizarPersonalizado(coluna, valor, id);
                 }
                 if (opcao == Opcao.CadastrarTelefone)
                 {
@@ -139,13 +155,13 @@ namespace CadastroPessoa
                     Console.WriteLine("-> Informe o nome da pessoa para vincular ao telefone:");
                     string nome = Console.ReadLine();
                     var pessoa = AcharPessoaPeloNome(nome);
-                    if(pessoa == null)
+                    if (pessoa == null)
                     {
                         Console.WriteLine("-> Informe um nome mais expecífico!");
                         nome = Console.ReadLine();
                         pessoa = AcharPessoaPeloNome(nome);
                     }
-                    else 
+                    else
                     {
 
                         Console.WriteLine("-> Informe o DDD:");
@@ -167,7 +183,7 @@ namespace CadastroPessoa
                         Console.WriteLine("-> Informe o id do telefone:");
                         int idtel = Convert.ToInt32(Console.ReadLine());
 
-                        var telefone = AcharTelefonePeloId(pessoa.Id,idtel);
+                        var telefone = AcharTelefonePeloId(idtel);
                         if (telefone != null)
                         {
                             Console.WriteLine("-> Informe o novo DDD:");
@@ -189,13 +205,68 @@ namespace CadastroPessoa
                 }
                 if (opcao == Opcao.ExcluirTelefone)
                 {
-                    Console.WriteLine("-> Antes de excluir um telefone, é necessário excluir o cadastro da pessoa!\n\n-> Informe o Id da pessoa a ser excluida:");
-                    int idpessoa = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("-> Informe o Id do telefone:");
                     int id = Convert.ToInt32(Console.ReadLine());
-                    //var telefone = AcharTelefonePeloId(id);
+                    var telefone = AcharTelefonePeloId(id);
+                    if (telefone != null)
+                    {
+                        ExcluirTelefone(id);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cadastro não encontrado!");
+                    }
+                }
+                if (opcao == Opcao.ListarTelefone)
+                {
+                    ListarTelefones();
+                }
+                if (opcao == Opcao.ListarTelefonePorPessoa)
+                {
+                    Console.WriteLine("-> Informe o Id da pessoa:");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    var pessoa = AcharPessoaPeloId(id);
+                    if (pessoa != null)
+                    {
+                        ListarTelefonePorPessoa(id,pessoa.Nome);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cadastro não encontrado!");
+                    }
+                }
+                if (opcao == Opcao.MostrarQuantidadeDeTelefone)
+                {
+                    int quantidade = QuantidadeDeTelefones();
+                    if (quantidade > 0)
+                    {
+                        Console.WriteLine("-> Quantidade de telefones cadastrados: "+ quantidade);
+                    }
+                    else
+                    {
+                        Console.WriteLine("-> Não há cadastros de telefones");
+                    }
+                }
+                if (opcao == Opcao.MostraQuantidadeDeTelefonePorPessoa)
+                {
+                    Console.WriteLine("-> Informe o Id da pessoa:");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    var pessoa = AcharPessoaPeloId(id);
+                    if (pessoa != null)
+                    {
+                        int quantidade = QuantidadeDeTelefonePorPessoa(id);
+                        if (quantidade > 0)
+                        {
+                            Console.WriteLine("-> Quantidade de telefones cadastrados para a pessoa : " + pessoa.Nome +" : "+ quantidade);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("-> Cadastro não encontrado:");
+                    }
                 }
             } while (opcao != Opcao.Sair);
-        }
+        } 
 
         static Pessoa CadastrarPessoa()
         {
@@ -223,10 +294,9 @@ namespace CadastroPessoa
                 {
 
                     string sql = "select Id,Nome,Cpf,Rg,Data_Nascimento,Naturalidade from Pessoa ORDER BY id";
-                    SqlCommand sqlComm = new SqlCommand(sql,cnn);;
+                    SqlCommand sqlComm = new SqlCommand(sql,cnn);
                     sqlComm.Connection.Open();
                     SqlDataReader rdr = sqlComm.ExecuteReader();
-
 
                     foreach (DbDataRecord s in rdr)
                     {
@@ -245,8 +315,6 @@ namespace CadastroPessoa
                         Console.WriteLine("Naturalidade: "+naturalidade);
                         Console.WriteLine("----------------------------");
                     }
-
-
                 }
             }
             catch (SqlException e)
@@ -344,7 +412,7 @@ namespace CadastroPessoa
 
 
         }
-        static Telefone AcharTelefonePeloId(int id,int idtel)
+        static Telefone AcharTelefonePeloId(int id)
         {
             var telefone = new Telefone();
 
@@ -354,10 +422,9 @@ namespace CadastroPessoa
                 using (var cnn = new SqlConnection(Coneccao))
                 {
 
-                    string sql = "select * from Telefone where IdPessoa = @id and Id = @idtel";
+                    string sql = "select * from Telefone where Id = @id";
                     SqlCommand sqlComm = new SqlCommand(sql, cnn);
                     sqlComm.Parameters.AddWithValue("@id", id);
-                    sqlComm.Parameters.AddWithValue("@idtel", idtel);
                     sqlComm.Connection.Open();
                     sqlComm.ExecuteNonQuery();
                     SqlDataReader rdr = sqlComm.ExecuteReader();
@@ -397,6 +464,183 @@ namespace CadastroPessoa
             {
                 Console.WriteLine(e.Errors);
             }
+        }
+        static void ListarTelefones()
+        {
+            try
+            {
+                string Coneccao = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Bruno;Data Source=ITELABD03\SQLEXPRESS01";
+                using (var cnn = new SqlConnection(Coneccao))
+                {
+
+                    string sql = "select t.Id,t.DDD,t.Numero,t.IdPessoa,p.Nome from Telefone t , Pessoa p where t.IdPessoa = p.Id";
+                    SqlCommand sqlComm = new SqlCommand(sql, cnn);
+                    sqlComm.Connection.Open();
+                    SqlDataReader rdr = sqlComm.ExecuteReader();
+
+                    foreach (DbDataRecord s in rdr)
+                    {
+
+                        int id = s.GetInt32(0);
+                        string ddd = s.GetString(1);
+                        string numero = s.GetString(2);
+                        int IdPessoa = s.GetInt32(3);
+                        string nome = s.GetString(4);
+                        Console.WriteLine("Nome: " + nome);
+                        Console.WriteLine("DDD: " + ddd);
+                        Console.WriteLine("Número : " + numero);
+                        Console.WriteLine("----------------------------");
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Errors);
+            }
+        }
+        static void ListarTelefonePorPessoa(int idPessoa,string nome)
+        {
+            try
+            {
+                string Coneccao = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Bruno;Data Source=ITELABD03\SQLEXPRESS01";
+                using (var cnn = new SqlConnection(Coneccao))
+                {
+
+                    string sql = "select t.Id,t.DDD,t.Numero,t.IdPessoa,p.Nome from Telefone t , Pessoa p where t.IdPessoa = @idPessoa and p.Nome = @nome";
+                    SqlCommand sqlComm = new SqlCommand(sql, cnn);
+                    sqlComm.Parameters.AddWithValue("@idPessoa",idPessoa);
+                    sqlComm.Parameters.AddWithValue("@nome",nome);
+                    sqlComm.Connection.Open();
+                    SqlDataReader rdr = sqlComm.ExecuteReader();
+
+                    foreach (DbDataRecord s in rdr)
+                    {
+
+                        int id = s.GetInt32(0);
+                        string ddd = s.GetString(1);
+                        string numero = s.GetString(2);
+                        int IdPessoa = s.GetInt32(3);
+                        string nome1 = s.GetString(4);
+                        Console.WriteLine("Nome: " + nome1);
+                        Console.WriteLine("DDD: " + ddd);
+                        Console.WriteLine("Número : " + numero);
+                        Console.WriteLine("----------------------------");
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Cadastro não encontrado!" + e.Errors);
+            }
+        }
+        static Telefone AcharTelefonePeloIdPessoa(int id)
+        {
+            var telefone = new Telefone();
+
+            try
+            {
+                string Coneccao = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Bruno;Data Source=ITELABD03\SQLEXPRESS01";
+                using (var cnn = new SqlConnection(Coneccao))
+                {
+
+                    string sql = "select * from Telefone where IdPessoa = @id";
+                    SqlCommand sqlComm = new SqlCommand(sql, cnn);
+                    sqlComm.Parameters.AddWithValue("@id", id);
+                    sqlComm.Connection.Open();
+                    sqlComm.ExecuteNonQuery();
+                    SqlDataReader rdr = sqlComm.ExecuteReader();
+                    rdr.Read();
+                    telefone.Id = rdr.GetInt32(0);
+                    telefone.DDD = rdr.GetString(1);
+                    telefone.Numero = rdr.GetString(2);
+                    telefone.IdPessoa = rdr.GetInt32(3);
+                }
+                return telefone;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+
+        }
+        static void ExcluirTelefoneIdPessoa(int id)
+        {
+            try
+            {
+                string Coneccao = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Bruno;Data Source=ITELABD03\SQLEXPRESS01";
+                using (var cnn = new SqlConnection(Coneccao))
+                {
+
+                    string sql = "delete from Telefone where IdPessoa = @id";
+
+                    SqlCommand sqlComm = new SqlCommand(sql, cnn);
+                    sqlComm.Parameters.AddWithValue("@id", id);
+                    sqlComm.Connection.Open();
+                    sqlComm.ExecuteNonQuery();
+                    Console.WriteLine("-> Cadastro excluido!");
+
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Errors);
+            }
+        }
+        static int QuantidadeDeTelefones()
+        {
+            int quantidade = 0;
+
+            try
+            {
+                string Coneccao = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Bruno;Data Source=ITELABD03\SQLEXPRESS01";
+                using (var cnn = new SqlConnection(Coneccao))
+                {
+
+                    string sql = "SELECT COUNT(id)FROM Telefone";
+                    SqlCommand sqlComm = new SqlCommand(sql, cnn);
+                    sqlComm.Connection.Open();
+                    sqlComm.ExecuteNonQuery();
+                    SqlDataReader rdr = sqlComm.ExecuteReader();
+                    rdr.Read();
+                    quantidade = rdr.GetInt32(0);
+
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Errors);
+            }
+
+            return quantidade;
+        }
+        static int QuantidadeDeTelefonePorPessoa(int id)
+        {
+            int quantidade = 0;
+
+            try
+            {
+                string Coneccao = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Bruno;Data Source=ITELABD03\SQLEXPRESS01";
+                using (var cnn = new SqlConnection(Coneccao))
+                {
+
+                    string sql = "SELECT COUNT(IdPessoa)FROM Telefone where IdPessoa = @id";
+                    SqlCommand sqlComm = new SqlCommand(sql, cnn);
+                    sqlComm.Parameters.AddWithValue("@id",id);
+                    sqlComm.Connection.Open();
+                    sqlComm.ExecuteNonQuery();
+                    SqlDataReader rdr = sqlComm.ExecuteReader();
+                    rdr.Read();
+                    quantidade = rdr.GetInt32(0);
+
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Errors);
+            }
+
+            return quantidade;
         }
     }
 }
